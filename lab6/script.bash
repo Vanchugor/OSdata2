08e6bc0d0a819a4c4b7848b0c8518cd7d5fce656
Fyodor Vanchugov <fyodor.vanchugov@waveaccess.ru>
2022-12-28 02:28:31 +0300

    Amend a for loop

--- lab6/script.bash
+++ lab6/script.bash
@@ -2,7 +2,7 @@
 poly=1.0
 sign=1.0
 result=1.0
-for i in {1..500000000}
+for (( i = 0; i < 500000000; i++ ))
 do
    poly=$(echo "$poly * $1" | bc -l)
    result=$(echo "$result + $sign * ($poly / $i)" | bc -l)
