3ba3601ab53bb2b1e2bb37057c719ee2f0bec7e9
Fyodor Vanchugov <fyodor.vanchugov@waveaccess.ru>
2022-12-28 02:44:51 +0300

    Amend bc expressions

--- lab6/script.bash
+++ lab6/script.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
-poly=1.0
-sign=1.0
-result=1.0
+poly="1.0"
+sign="1.0"
+result="1.0"
 for (( i = 0; i < 500000000; i++ ))
 do
    poly=$(echo "$poly*$1" | bc -l)
-   result=$(echo "$result+$sign*($poly/$i)" | bc -l)
-   sign=$(echo "-1.0*$sign" | bc -l)
+   result=$(echo "$result+$sign*$poly/$i" | bc -l)
+   sign=$(echo "-$sign" | bc -l)
 done
 
 echo "$result"
