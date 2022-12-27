f1d6866f99bf534eaed61808aef40220c5521e83
Fyodor Vanchugov <fyodor.vanchugov@waveaccess.ru>
2022-12-28 02:37:02 +0300

    Amend bc

--- lab6/script.bash
+++ lab6/script.bash
@@ -4,9 +4,9 @@ sign=1.0
 result=1.0
 for (( i = 0; i < 500000000; i++ ))
 do
-   poly=$(echo "$poly * $1" | bc -l)
-   result=$(echo "$result + $sign * ($poly / $i)" | bc -l)
-   sign=$(echo "-1.0 * $sign" | bc -l)
+   poly=$(echo "$poly*$1" | bc -l)
+   result=$(echo "$result+$sign*($poly/$i)" | bc -l)
+   sign=$(echo "-1.0*$sign" | bc -l)
 done
 
 echo "$result"
