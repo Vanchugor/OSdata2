3918491e448d7dfbdc45fce118ec3ea0c744755a
Fyodor Vanchugov <fyodor.vanchugov@waveaccess.ru>
2022-12-28 17:43:46 +0300

    Fix data collecting

--- lab6/start.bash
+++ lab6/start.bash
@@ -3,9 +3,11 @@
 echo -n "" > sc_data
 for (( N = 0; N < 20; N++ ))
 do
+  echo "**** CURRENT N = $N **** "
   echo "**** CURRENT N = $N **** " >> sc_data
   for (( t = 0; t < 10; t++ ))
   do
-    { time bash launcher.bash $N; } >> sc_data
+    echo "** CURRENT t = $t ** "
+    { time bash launcher.bash $N; } 2>> sc_data
   done
 done
