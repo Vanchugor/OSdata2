2071323ad1b426c30bd495b2fd6293f52214caf5
Fyodor Vanchugov <fyodor.vanchugov@waveaccess.ru>
2022-12-28 16:00:50 +0300

    Try to fix bc

--- lab6/script.bash
+++ lab6/script.bash
@@ -5,12 +5,12 @@ result="0.0"
 for (( i = 1; i < 500000000; i++ ))
 do
    poly=$(echo "$poly*$1" | tr -d $'\r' | bc -l)
-   echo "PLY: $poly"
+#   echo "PLY: $poly"
    result=$(echo "$result+$sign*$poly/$i" | tr -d $'\r' | bc -l)
-   echo "RES: $result"
+#   echo "RES: $result"
    sign=$(echo "-1.0*$sign" | tr -d $'\r' | bc -l)
-   echo "SIG: $sign"
-   echo "------------------------"
+#   echo "SIG: $sign"
+#   echo "------------------------"
 done
 
 echo "$result"
