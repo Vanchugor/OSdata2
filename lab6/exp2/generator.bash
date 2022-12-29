b79fa7a4f8e8ca54e4b4c09ca862558bb28dcd2e
Fyodor Vanchugov <fyodor.vanchugov@waveaccess.ru>
2022-12-29 22:28:22 +0300

    Bugfix in generator.bash

--- lab6/exp2/generator.bash
+++ lab6/exp2/generator.bash
@@ -4,11 +4,11 @@ for (( t = 1; t < 6; t++ ))
   do
     for (( i = "$(( $t + 5 ))"; i < 21; i = i + 5 )); do
         mkdir "data_$i"
-        cp "data_$t/data_"$t"_t1" "data_$i/data_"$i"_t1"
-        cp "data_$t/data_"$t"_t2" "data_$i/data_"$i"_t2"
-        cp "data_$t/data_"$t"_t3" "data_$i/data_"$i"_t3"
-        cp "data_$t/data_"$t"_t4" "data_$i/data_"$i"_t4"
-        cp "data_$t/data_"$t"_t5" "data_$i/data_"$i"_t5"
+        cp "data_$t/data_"$t"_t1.txt" "data_$i/data_"$i"_t1.txt"
+        cp "data_$t/data_"$t"_t2.txt" "data_$i/data_"$i"_t2.txt"
+        cp "data_$t/data_"$t"_t3.txt" "data_$i/data_"$i"_t3.txt"
+        cp "data_$t/data_"$t"_t4.txt" "data_$i/data_"$i"_t4.txt"
+        cp "data_$t/data_"$t"_t5.txt" "data_$i/data_"$i"_t5.txt"
     done
   done
 
