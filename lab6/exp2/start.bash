011adb95d5cd08d24de1f252413a8b5ed15f1ffb
Fyodor Vanchugov <fyodor.vanchugov@waveaccess.ru>
2022-12-30 01:39:37 +0300

    Prepare for experiments again

--- lab6/exp2/start.bash
+++ lab6/exp2/start.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo -n "" > sc_data.txt
-for (( file_num = 1; file_num < 2; file_num++ ))
+for (( file_num = 1; file_num < 21; file_num++ ))
 do
   echo "**** CURRENT file_num = $file_num **** "
   echo "**** CURRENT file_num = $file_num **** " >> sc_data.txt
-  for (( t = 1; t < 2; t++ ))
+  for (( t = 1; t < 6; t++ ))
   do
     echo "** CURRENT t = $t ** "
 #    { time bash handler.bash $t $file_num; } 2>> sc_data.txt &
