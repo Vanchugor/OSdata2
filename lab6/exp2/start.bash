5b2796627c70d03a9815856a592249cafd697e99
Fyodor Vanchugov <fyodor.vanchugov@waveaccess.ru>
2022-12-30 02:05:10 +0300

    Make parallel

--- lab6/exp2/start.bash
+++ lab6/exp2/start.bash
@@ -8,8 +8,8 @@ do
   for (( t = 1; t < 6; t++ ))
   do
     echo "** CURRENT t = $t ** "
-#    { time bash handler.bash $t $file_num; } 2>> sc_data.txt &
-    { time bash handler.bash $t $file_num; } 2>> sc_data.txt
+    { time bash handler.bash $t $file_num; } 2>> sc_data.txt &
+#    { time bash handler.bash $t $file_num; } 2>> sc_data.txt
   done
-#  wait
+  wait
 done
